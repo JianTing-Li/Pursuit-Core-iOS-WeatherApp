7235479ad71c6c2aa9e9f13062299e8c1538c4e7
Jian Ting Li <jiantingli@Jian-Tings-MacBook-Pro.local>
2019-01-20 14:28:29 -0500

    pixaba API model setup for JSON Parsing

--- WeatherApp/WeatherApp/Image.swift
+++ WeatherApp/WeatherApp/Image.swift
@@ -9,5 +9,18 @@
 import Foundation
 
 struct Image: Codable {
-    
+    struct ImageData: Codable {
+        let hits: [Image]
+    }
+    let largeImageURL: URL
+    let webformatHeight: Int
+    let webformatWidth: Int
+    let imageWidth: Int
+    let imageHeight: Int
+    let webformatURL: URL
+    let type: String          // "photo"
+    let previewHeight: Int
+    let tags: String          // "new york city, brooklyn bridge, night",
+    let previewWidth: Int
+    let previewURL: URL
 }
